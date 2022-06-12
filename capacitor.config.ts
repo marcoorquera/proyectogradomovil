26bdbecd138366585f5fc26037211e8a0eaae7e7
marcoorquera <75541559+marcoorquera@users.noreply.github.com>
2022-06-12 14:04:09 -0500

    add icon and fix styles in cart

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.muebleria.global',
-  appName: 'muebleriaApp',
+  appName: 'Global Muebles',
   webDir: 'www',
   bundledWebRuntime: false
 };
